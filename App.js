9003d8b29214b5837d7cfe0237bdca75cb463923
IntoKanto <anatoli.tselms@myy.haaga-helia.fi>
2022-10-12 19:32:07 +0300

    Voices

--- App.js
+++ App.js
@@ -1,10 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, ListViewBase, StyleSheet, Text, TextInput, View } from 'react-native';
+import * as Speech from 'expo-speech';
+import { useEffect, useState } from 'react';
 
 export default function App() {
+  const [text, setText] = useState('')
+  const [voices, setVoices] = useState([])
+
+  
+
+ 
+  
+  const speakText = () => {    
+   let options = {
+      voice: 'com.apple.eloquence.en-GB.Grandpa'
+    }
+    Speech.speak(text, options)
+
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <TextInput style={styles.input}
+      value={text}
+      onChangeText={text => setText(text)}
+      />
+      <Button 
+      title='Speak'
+      onPress={speakText}
+      />
+     
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +43,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    borderColor: 'blue',
+    borderWidth: 1,
+    width: 150
+  }
 });
